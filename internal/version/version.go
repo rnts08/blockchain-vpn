a1795d3b5363ad0f86068df377dae88418100ef8
z <timh.bergstrom@gmail.com>
2026-03-12 15:38:44 +0700

    Bump version to 0.5.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var (
 	// Application semantic version.
-	Version = "0.5.1"
+	Version = "0.5.2"
 	// Optional build metadata (can be injected via -ldflags).
 	GitCommit = "dev"
 	BuildDate = "unknown"
